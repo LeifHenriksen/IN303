cedf53c7a301b1021f2a406b42dcfcf4bbc66929
Beoffre <41382995+Beoffre@users.noreply.github.com>
2018-11-22 20:51:36 +0100

    Add files via upload
    compile, puis lance juste ton fichier avec test comme argv[1]

---
+++ huff.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct occuCharac{
+    int occu;
+    int codeAscii;
+};
+
+int* tabOccu(char* fileName){
+    FILE *myFile=fopen(fileName, "r");
+    
+    if(myFile==NULL){
+        exit(2);        
+    }
+    int charac;
+    int* tab=malloc(256);
+    while ((charac=fgetc(myFile))!=EOF){
+        tab[charac]++;
+    }
+    return(tab);
+}
+
+
+struct occuCharac* tabStruct(int* tabInt){
+    int nbrCases=0;
+    for(int i=0; i<256;i++){
+        if (tabInt[i]!=0){
+            nbrCases++;
+        }
+    }
+    struct occuCharac* laTabStruct=malloc(nbrCases);
+    int compteurCases=0;
+    for(int i=0;i<256;i++){
+        if (tabInt[i]!=0){
+            laTabStruct[compteurCases].occu=tabInt[i];
+            laTabStruct[compteurCases].codeAscii=i;
+            printf("%i fois '%c' \n",tabInt[i],(char)i);
+            compteurCases++;
+        }
+    }
+    return(laTabStruct);
+}
+
+
+int main(int argc , char** argv){
+    tabStruct(tabOccu(argv[1]));
+    return(0);
+}
